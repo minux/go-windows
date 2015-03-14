15af3a0a18b5174bddd66a12d462f80587e0c33a
Shenghou Ma <minux@golang.org>
2015-03-13 22:01:31 -0400

    cmd/dist/test.go: enable external linking tests on windows/amd64
    Signed-off-by: Shenghou Ma <minux@golang.org>

--- src/cmd/dist/test.go
+++ src/cmd/dist/test.go
@@ -357,7 +357,7 @@ func (t *tester) extLink() bool {
 		"linux-386", "linux-amd64", "linux-arm",
 		"netbsd-386", "netbsd-amd64",
 		"openbsd-386", "openbsd-amd64",
-		"windows-386":
+		"windows-386", "windows-amd64":
 		return true
 	case "darwin-386", "darwin-amd64":
 		// linkmode=external fails on OS X 10.6 and earlier == Darwin
@@ -374,12 +374,6 @@ func (t *tester) extLink() bool {
 
 func (t *tester) cgoTest() error {
 	env := mergeEnvLists([]string{"GOTRACEBACK=2"}, os.Environ())
-
-	if t.gohostos == "windows" && t.gohostarch != "386" {
-		cmd := t.dirCmd("misc/cgo/test", "go", "test")
-		cmd.Env = env
-		return cmd.Run()
-	}
 
 	if t.gohostos != "dragonfly" {
 		// linkmode=internal fails on dragonfly since errno is a TLS relocation.
@@ -400,7 +394,7 @@ func (t *tester) cgoTest() error {
 			return err
 		}
 	case "darwin-386", "darwin-amd64",
-		"windows-386":
+		"windows-386", "windows-amd64":
 		if t.extLink() {
 			cmd := t.dirCmd("misc/cgo/test", "go", "test", "-ldflags", "-linkmode=external")
 			cmd.Env = env
